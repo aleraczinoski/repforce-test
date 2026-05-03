4f42fd90ebe20af4f76c427c541acbb70e010b6f
Alexandre Raczinoski <alexandreraczinoski@gmail.com>
2026-05-03 14:22:40 -0300

    fix: adjust cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule); // Cria a aplicação NestJS
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-  }); //Conexão front e back -> Back aceita requisições vindas da porta 5173
+    origin: ['http://localhost:5173', 'https://repforce-test.netlify.app'],
+  }); // Permite requisições do frontend rodando localmente e do deploy na Netlify
 
   await app.listen(3000); // Back roda na porta 3000
 }
